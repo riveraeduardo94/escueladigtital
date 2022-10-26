479c8b12773947b5bb60bfb20693d20e39e2cfe7
Eduardo Rivera <lic.riveraeduard@gmail.com>
2022-10-25 20:03:23 -0500

    Feat: php

--- php/enviar.php
+++ php/enviar.php
@@ -1,13 +1,23 @@
 <?php
-$destino = "hazardeddy1994@gmail.com";
-$nombre = $_POST["nombre"];
-$correo = $_POST["correo"];
-$telefono = $_POST["telefono"];
-$asunto = $_POST["asunto"];
-$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nAsunto" . $asunto;
+if ( ! isset( $_POST['nombre'] ) ) {
+    header("Location: gracias.html");
+}
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$teléfono = $_POST['teléfono'];
+$asunto = $_POST['asunto'];
+
+//$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nAsunto" . $asunto;
+//$destino = "hazardeddy1994@gmail.com";
+
+var_dump($nombre);
+
+$rta = mail( 'hazardeddy1994@gmail.com', "$nombre", $asunto, $headers );
+var_dump($rta);
 
 //Destino
-mail($destino, "Contacto", $contenido);
-header("Location:gracias.html")
+//mail($destino, "Contacto", $contenido);
+header("Location: gracias.html" );
 
 ?>
